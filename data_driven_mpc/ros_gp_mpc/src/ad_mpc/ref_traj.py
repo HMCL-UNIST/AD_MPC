fix wrap_pi numerical issue

data_driven_mpc/ros_gp_mpc/src/ad_mpc/ref_traj.py
@@ -19,7 +19,8 @@ def compute_curvature(cdists, psis):
 	if len(curv_raw) > 33:
 		curv_filt = filtfilt(np.ones((11,))/11, 1, curv_raw) # curvature filter suggested by Jinkkwon Kim.
 	else:
-		curv_filt = curv_raw
+		curv_filt = filtfilt(np.ones((11,))/11, 1, curv_raw, padlen=3)
+		print("pad ing 3@!!!!!!!!!!!!!!")
 	# Curvature Filtering: (https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.filtfilt.html)
 	
 	return curv_filt
@@ -90,7 +91,7 @@ class RefTrajectory():
 		if self.access_map is None:
 			rospy.loginfo("trajectory has not been set")
 			return
-		psi_init = bound_angle_within_pi(psi_init)
+		# psi_init = bound_angle_within_pi(psi_init)
 
 		waypoint_dict = {}
 		xy_traj = self.trajectory[ :, [self.access_map['x'], self.access_map['y']] ] # XY trajectory
@@ -124,7 +125,7 @@ class RefTrajectory():
 		start_dist = self.trajectory[closest_index, self.access_map['cdist']]			
 		interp_by_key = 'cdist'
 		while len(vel_references) < self.traj_horizon+1:
-			vel_references = np.concatenate((vel_references, [0.0001]))
+			vel_references = np.concatenate((vel_references, [0.01]))
 		interp_to_fit = [h*self.traj_dt*vel_references[h] + start_dist for h in range(1, self.traj_horizon+1)]
 		
 		for waypoint_key in ['x', 'y', 'psi', 'cdist', 'curv']:

data_driven_mpc/ros_gp_mpc/src/utils/utils.py
@@ -840,16 +840,18 @@ def separate_variables(traj):
     r_traj = traj[:, 10:]
     return [p_traj, a_traj, v_traj, r_traj]
 
+def bound_angle_within_pi(angle):
+	return (angle + np.pi) % (2.0 * np.pi) - np.pi
 
 def wrap_to_pi(angle):
     """
     :param angle: (float)
     :return: (float) Angle in radian in [-pi, pi]
     """
-    while angle > np.pi:
+    while angle > np.pi-0.01:
         angle -= 2.0 * np.pi
 
-    while angle < -np.pi:
+    while angle < -np.pi+0.01:
         angle += 2.0 * np.pi
 
     return angle 