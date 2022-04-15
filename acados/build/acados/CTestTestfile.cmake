76b86150bc8b15dbecb4cec9acf4b5b0fccb8a44
amilearning <hojin.projects@gmail.com>
2022-04-15 11:14:38 +0900

    remove acados

--- acados/build/acados/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hojin/ADplatform/hmclmobil/src/acados/acados
-# Build directory: /home/hojin/ADplatform/hmclmobil/src/acados/build/acados
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
